43a2499c58247c0ea4e0e7db062b3c98ae609710
cesar-limachi <limachi86@gmail.com>
2021-10-15 05:15:04 -0500

    finalizado

--- lab07/tienda/views.py
+++ lab07/tienda/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import get_object_or_404, render
-
 from .models import Producto, Categoria
 # Create your views here.
 
@@ -21,3 +20,29 @@ def categoria(request,categoria_id):
     categoria = Categoria.objects.all()
     producto = Producto.objects.filter(categoria_id=categoria_id)
     return render(request,'categoria.html',{'categoria':categoria,'producto':producto})
+
+from tienda.carrito import Cart
+
+def agregarCarrito(request,producto_id):
+    objProducto = Producto.objects.get(id=producto_id)
+    carritoProducto = Cart(request)
+    carritoProducto.add(objProducto,1)
+    print(request.session.get("cart"))
+    return render(request,'carrito.html')
+
+def eliminarProductoCarrito(request,producto_id):
+    objProducto = Producto.objects.get(id=producto_id)
+    carritoProducto = Cart(request)
+    carritoProducto.remove(objProducto)
+    print(request.session.get("cart"))
+    return render(request,'carrito.html')
+
+def limpiarCarrito(request):
+    CarritoProducto = Cart(request)
+    CarritoProducto.clear()
+    print(request.session.get("cart"))
+    return render(request,'carrito.html')
+
+def carrito(request):
+    print(request.session.get("cart"))
+    return render(request,'carrito.html')
